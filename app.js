7dc557d6ef25fad4cb7b7c2d7040a6d0565505d1
Scott A. Lawrence <slawrence@gmail.com>
2018-01-29 22:00:05 -0500

    Hardcoded call to Dark Sky API

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode')
 
@@ -19,5 +20,16 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     console.log(errorMessage);
   } else {
     console.log(JSON.stringify(results, undefined, 2));
+  }
+});
+
+request({
+  url: `https://api.darksky.net/forecast/a870a33fe1882f79763b3ffca5da4fd6/39.0939518,-77.0213375`,
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200){
+    console.log(`Current temperature at Mom & Dad's: ${body.currently.temperature}F`);
+  } else {
+    console.log('Unable to fetch weather')
   }
 });
